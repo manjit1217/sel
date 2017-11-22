7ee43465a82b2846367c98d6d75a2342d3fc1b8b
Manjit <manjit.1217@gmail.com>
2017-11-22 23:11:42 +0530

    up

---
+++ src/sel/proprun.java
@@ -0,0 +1,17 @@
+/*	package sel;
+
+import java.io.IOException;
+
+public class proprun extends testbase {
+	
+	public void testproprties() throws IOException
+	{
+		
+	
+ String name = getobject("username");
+ //System.out.println(name);
+ 
+}
+}
+
+*/
